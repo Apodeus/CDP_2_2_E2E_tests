8f78f3a82ce4e7e9775a516f97dd9de8d775d863
Romain Ordonez <romain.ordonez@u-bordeaux.fr>
2018-11-08 09:03:39 +0000

    Update table_users.sql

--- src/db/table_users.sql
+++ src/db/table_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE users(
   id INTEGER PRIMARY KEY NOT NULL,
   pseudo VARCHAR(20) NOT NULL,
   email VARCHAR(60) NOT NULL,
-  password VARCHAR(20) NOT NULL
+  password VARCHAR2(20) NOT NULL
 );
 
-INSERT INTO users (id, pseudo, email, password) values (0, "admin", "admin", "admin");
+INSERT INTO users (id, pseudo, email, password) values (0, "admin", "admin@admin.com", "admin");
