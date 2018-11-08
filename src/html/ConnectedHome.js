07935a9bbe2dc0ae989bec1b4b2981bf86790a4f
cfontenay <clementfontenay@gmail.com>
2018-11-09 00:38:16 +0100

    nettoyage code

--- src/html/ConnectedHome.js
+++ src/html/ConnectedHome.js
@@ -3,11 +3,10 @@ function clickButtonMesProjets(){
     url:"/?MesProjets=1",
     type:"GET",
     success: function(data){
-        console.log(data);
         window.location = data;
     },
     error: function(request, msg, error){
-        alert("error delete" + data);
+	console.log(error);
     }
 });
 
