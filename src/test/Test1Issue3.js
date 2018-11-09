dc5b9730d264cf78ddf2b7de38fd0c946cec39a5
cfontenay <clementfontenay@gmail.com>
2018-11-09 15:36:16 +0100

    ajout test E2E issue 3

---
+++ src/test/Test1Issue3.js
@@ -0,0 +1,42 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async function test() {
+  let driver = await new Builder().forBrowser('safari').build();
+  try {
+    let home = 'http://localhost:3000';
+    await driver.get(home);
+    await driver.findElement(By.id('MesProjets')).click().then( () => console.log("click"));
+    await driver.sleep(1000);
+    let url = await driver.getCurrentUrl();
+    if (home == url){
+      return -1;
+    }
+    await driver.findElement(By.tagName("h2")).then(function(e) {
+      if (e == undefined)
+        return -1;
+    });
+    
+    await driver.navigate().back();
+    await driver.sleep(2000);
+    url = await driver.getCurrentUrl();
+    if (home != url){
+      return -1;
+    }
+    await driver.findElement(By.id('MesProjets')).click().then( () => console.log("click"));
+    await driver.sleep(1000);
+    url = await driver.getCurrentUrl();
+    if (home == url){
+      return -1;
+    }
+    await driver.findElement(By.tagName("h2")).then(function(e) {
+      if (e == undefined)
+        return -1;
+    });
+    
+
+  } 
+  finally {
+    await driver.quit();
+  }
+  
+})();
