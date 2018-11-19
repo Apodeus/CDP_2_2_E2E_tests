a15e2856e457d75e3e04b8391dbccbe2c0e0f611
Romain Ordonez <romain.ordonez@u-bordeaux.fr>
2018-11-19 14:40:56 +0000

    Update e2e.js

--- src/test/e2e.js
+++ src/test/e2e.js
@@ -2,7 +2,10 @@ module.exports = {
     'Authentication succes' : function (client) {
         client
             .url('http://app:3000/')
-            .waitForElementVisible("button[id='MesProjets']", 1000)
+            .source(function(result) {
+                console.log(result.value);
+            })
+            .waitForElementVisible("button[id='MesProjets']", 2000)
             .click("button[id='MesProjets']")
             .end();
     }
