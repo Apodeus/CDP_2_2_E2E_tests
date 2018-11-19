9b32da9d80f86eefbb4e7560fd9bb44d89a70a65
Romain Ordonez <romain.ordonez@u-bordeaux.fr>
2018-11-19 14:34:18 +0000

    Fix du test témoin e2e.js

--- src/test/e2e.js
+++ src/test/e2e.js
@@ -2,7 +2,7 @@ module.exports = {
     'Authentication succes' : function (client) {
         client
             .url('http://app:3000/')
-            .waitForElementVisible('body', 1000)
+            .waitForElementVisible("button[id='MesProjets']", 1000)
             .click("button[id='MesProjets']")
             .end();
     }
