af2d49a1a3904bc6b936744b70c95ffb891fe27f
romain <ordonez.romain@gmail.com>
2018-11-19 16:11:37 +0100

    Fixing e2e.js

--- src/test/e2e.js
+++ src/test/e2e.js
@@ -1,7 +1,11 @@
 module.exports = {
     'Authentication succes' : function (client) {
         client
-            .url('http://app:3000/')
+            .url('http://localhost:3000/')
+            .source(function(result) {
+                console.log(result.value);
+            })
+	    .url('http://app:3000')
             .source(function(result) {
                 console.log(result.value);
             })
