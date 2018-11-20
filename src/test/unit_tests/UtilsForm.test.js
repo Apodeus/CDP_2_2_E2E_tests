2dfeedf99854dc21aad278813a7063aca14d2c14
cfontenay <clementfontenay@gmail.com>
2018-11-20 15:19:19 +0100

    changement de lambda vers fonction, pour voir si le coverage marche mieux

--- src/test/unit_tests/UtilsForm.test.js
+++ src/test/unit_tests/UtilsForm.test.js
@@ -4,8 +4,7 @@ const UtilsForm = require('../../app/UtilsForm');
 const pathNameFiles = '/../../html/CreateProject';
 
 test('html form correctly built', async () => {
-  console.log('ah1');
-  await(async () => {
+  await (async function run() {
     jsdom.JSDOM.fromFile(path.resolve(__dirname+pathNameFiles+'.html'), '').then(async (dom) => {
       await (new UtilsForm()).addFormCreateProjectToDocument(dom);
       expect(dom.getElementById('name') != undefined);
