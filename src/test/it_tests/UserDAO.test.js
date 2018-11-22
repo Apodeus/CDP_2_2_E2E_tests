09eb26b564bb0bad89eb3878113726c305e646e4
Apodeus <ordonez.romain@gmail.com>
2018-11-22 17:20:42 +0100

    Ajout des tests E2E & IT

--- src/test/it_tests/UserDAO.test.js
+++ src/test/it_tests/UserDAO.test.js
@@ -3,80 +3,77 @@ const UserDAO = require('../../app/UserDAO');
 const util = require('util');
 const mysql = require('mysql2');
 
+async function newQuery(connectionDB, request){
+    await connectionDB.execute(request);
+}
+
+async function clearDatabase(connectionDB){
+    await Promise.all([
+        newQuery(connectionDB, "SET FOREIGN_KEY_CHECKS = 0"),
+        newQuery(connectionDB, "TRUNCATE TABLE projects_participants"),
+        newQuery(connectionDB, "TRUNCATE TABLE projects"),
+        newQuery(connectionDB, "DELETE FROM users WHERE id != 1"),
+        newQuery(connectionDB, "SET FOREIGN_KEY_CHECKS = 1"),
+    ]);
+}
+
 describe('Test DAO', () => {
 
-  const connectionDB = mysql.createConnection({
-    host: '0.0.0.0',
-    database: 'cdp',
-    pool: {
-      max: 50,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-    user: 'user',
-    password: 'root',
-  });
-
-  let dao;
-
-  beforeEach(async () => {
-    //jest.setTimeout(10000);
-    //await clearDatabase(connectionDB);
-    console.log("before end");
-    dao = new UserDAO(connectionDB);
-
-  });
-
-  test('it_should_insert_one_user', async () => {
-    expect(5).toBe(5);
-    await dao.getUserByName("user", (usr) => {
-      console.log(usr.id);
-      expect(usr.id).toBe(1);
-      expect(usr.pseudo).toBe("user");
+    const connectionDB = mysql.createConnection({
+        host: 'localhost',
+        database: 'cdp',
+        pool: {
+            max: 50,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        },
+        user: 'user',
+        password: 'root',
     });
-  });
 
-  afterAll(async () => {
-    connectionDB.close();
-  });
-    //given a user
-/*
-    const userDAO = new UserDAO(connectionDB);
-    const name = "name_test";
-    const email = "email@test.com";
-    const password = "password_test";
-    const user = new User(name, email, password);
-    //when saving this user in database
-    let savedUser = userDAO.save(user);
-    //then i should get back with the same information and an id
-    //expect(savedUser.id).toBe(1);
-      let resultUser = await userDAO.getUserByName(name, (usr) => {
-      expect(usr.pseudo).toBe(name);
-      expect(usr.email).toBe(email);
-      expect(usr.password).toBe(password);
+    let dao;
+
+    beforeEach(async () => {
+        jest.setTimeout(10000);
+        await clearDatabase(connectionDB);
+        dao = new UserDAO(connectionDB);
+
     });
-  });
-*/
+
+    test('it_should_return_one_user', async () => {
+        await dao.getUserByName("user", (usr) => {
+            console.log(usr.id);
+            expect(usr.id).toBe(1);
+            expect(usr.pseudo).toBe("user");
+            expect(usr.email).toBe("user@gmail.com");
+            expect(usr.password).toBe("user");
+        });
+    });
+
+    test('it_should_save_one_user', async () => {
+        const name = "name_test";
+        const email = "email@test.com";
+        const password = "password_test";
+        const user = new User(name, email, password);
+        //when saving this user in database
+        dao.save(user, (usr) => {
+            expect(usr.id).not.toBe(null);
+            expect(usr.pseudo).toBe(name);
+            expect(usr.email).toBe(email);
+            expect(usr.password).toBe(password);
+        });
+        //then i should get back with the same information and an id
+        let resultUser = await dao.getUserByName(name, (usr) => {
+            expect(usr.pseudo).toBe(name);
+            expect(usr.email).toBe(email);
+            expect(usr.password).toBe(password);
+        });
+    });
 
 
-async function clearDatabase(connectionDB) {
-  const connection = connectionDB;
-  const query = util.promisify(connection.query).bind(connection);
-  await ( async () => {
-    try {
-      await query("SET FOREIGN_KEY_CHECKS = 0", function(err, res) {});
-      await query("TRUNCATE TABLE users", function(err, result){
-        if(err){
-          throw err;
-        }
-        console.log("Truncate users success !");
-      });
-      await query("SET FOREIGN_KEY_CHECKS = 1", function(err, res) {});
-    } catch (err){
-      throw err;
-    }
-  })();
-}
+    afterAll( () => {
+        connectionDB.close();
+    });
 
 });
