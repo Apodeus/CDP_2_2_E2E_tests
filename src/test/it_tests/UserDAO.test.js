f88223256e3e47f31c33382b5c1c80091d2d4eb5
cfontenay <clementfontenay@gmail.com>
2018-11-22 18:06:07 +0100

    correction et amélioration du test sur UserDao pour pas qu'i l dépende du premier utilisateur de la base

--- src/test/it_tests/UserDAO.test.js
+++ src/test/it_tests/UserDAO.test.js
@@ -3,22 +3,21 @@ const UserDAO = require('../../app/UserDAO');
 const util = require('util');
 const mysql = require('mysql2');
 
-async function newQuery(connectionDB, request){
+async function newQuery(connectionDB, request) {
   await connectionDB.execute(request);
 }
 
-async function clearDatabase(connectionDB){
+async function clearDatabase(connectionDB) {
   await Promise.all([
-    newQuery(connectionDB, "SET FOREIGN_KEY_CHECKS = 0"),
-    newQuery(connectionDB, "TRUNCATE TABLE projects_participants"),
-    newQuery(connectionDB, "TRUNCATE TABLE projects"),
-    newQuery(connectionDB, "DELETE FROM users WHERE id != 1"),
-    newQuery(connectionDB, "SET FOREIGN_KEY_CHECKS = 1"),
+    newQuery(connectionDB, 'SET FOREIGN_KEY_CHECKS = 0'),
+    newQuery(connectionDB, 'TRUNCATE TABLE projects_participants'),
+    newQuery(connectionDB, 'TRUNCATE TABLE projects'),
+    newQuery(connectionDB, 'DELETE FROM users WHERE id != 1'),
+    newQuery(connectionDB, 'SET FOREIGN_KEY_CHECKS = 1'),
   ]);
- }
+}
 
 describe('Test DAO', () => {
-
   const connectionDB = mysql.createConnection({
     host: 'localhost',
     database: 'cdp',
@@ -31,49 +30,50 @@ describe('Test DAO', () => {
     user: 'user',
     password: 'root',
   });
-
   let dao;
-
   beforeEach(async () => {
     jest.setTimeout(10000);
-    await clearDatabase(connectionDB);
     dao = new UserDAO(connectionDB);
-
   });
+  clearDatabase(connectionDB);
 
-  test('it_should_return_one_user', async () => {
-    await dao.getUserByName("user", (usr) => {
-      console.log(usr.id);
-      expect(usr.id).toBe(1);
-      expect(usr.pseudo).toBe("user");
-      expect(usr.email).toBe("user@gmail.com");
-      expect(usr.password).toBe("user");
-    });
-  });
-
+  const name = 'name_test';
+  const email = 'email@test.com';
+  const password = 'password_test';
   test('it_should_save_one_user', async () => {
-    const name = "name_test";
-    const email = "email@test.com";
-    const password = "password_test";
-    const user = new User(name, email, password);
-    //when saving this user in database
-    dao.save(user, (usr) => {
+    const user = await new User(name, email, password);
+    // when saving this user in database
+    await dao.save(user, (usr) => {
       expect(usr.id).not.toBe(null);
       expect(usr.pseudo).toBe(name);
       expect(usr.email).toBe(email);
       expect(usr.password).toBe(password);
     });
-    //then i should get back with the same information and an id
-    let resultUser = await dao.getUserByName(name, (usr) => {
+  });
+
+  test('it_should_return_one_user', async () => {
+    // then i should get back with the same information and an id
+    await dao.getUserByName(name, (usr) => {
       expect(usr.pseudo).toBe(name);
       expect(usr.email).toBe(email);
       expect(usr.password).toBe(password);
     });
   });
 
-
-  afterAll( () => {
+  test('it_should_edit_one_user', async () => {
+    await dao.getUserByName(name, (user) => {
+      const saveID = user.id;
+      user.pseudo = 'new name';
+      dao.save(user, (usr) => {
+        expect(usr.id).toBe(saveID);
+        expect(usr.pseudo).toBe('new name');
+        expect(usr.email).toBe(email);
+        expect(usr.password).toBe(password);
+      });
+    });
+    // when saving this user in database
+  });
+  afterAll( async () => {   
     connectionDB.close();
   });
-
 });
