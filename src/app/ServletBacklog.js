e7ef91c2eabfaa8f3c9c60b08038fb8f868ced89
cfontenay <clementfontenay@gmail.com>
2018-12-03 17:30:11 +0100

    fin tâche Tc15.4i

--- src/app/ServletBacklog.js
+++ src/app/ServletBacklog.js
@@ -7,16 +7,26 @@ const TabBuilder = require('./TabBuilder');
 const Home = require('./ServletConnectedHome');
 const USDAO = require('./USDAO');
 const ProjectDAO = require('./ProjectDAO');
+const UtilsForm = require('./UtilsForm');
+const US = require('./US');
 
 
 const valueButtonCreateUS = 'Ajouter une nouvelle issue';
+const valueButtonDelete='Supprimer';
 
 router.get('/', async function(req, res) {
   await sendPage(res, res);
   res.end();
 });
 router.post('/', async function(req, res) {
-  module.exports.projectOpened = null;
+  if (req.body.delete === valueButtonDelete) {
+    const idUs = parseInt(req.body.data);
+    const usToDelete= new US('', '', 0, 0, 0, 0);
+    usToDelete.id = idUs;
+    await (new USDAO(Home.connectionDB).delete(usToDelete));
+  } else {
+    module.exports.projectOpened = null;
+  }
   await sendPage(res, req);
   res.end();
 });
@@ -27,22 +37,33 @@ async function sendPage(res, req) {
       await (new TabBuilder()).build(dom.window.document);
       const indexProject = module.exports.projectOpened === null ?
       parseInt(req.body.data):module.exports.projectOpened.id;
-      const projects = (await (new ProjectDAO(Home.connectionDB))
-          .getAllByUser(Home.connectedUser));
+      const projects = (await (new ProjectDAO(Home.connectionDB)).getAllByUser(Home.connectedUser));
       const projectIndex= await projects.findIndex((p) => p.id === indexProject);
       module.exports.projectOpened=projects[projectIndex];
       await addUSListToHtml(dom.window.document);
       await addButtonCreate(dom.window.document);
-      res.write(dom.serialize());
+      await res.write(dom.serialize());
     }
   });
 }
 async function addUSListToHtml(document) {
   const listUS = await (new USDAO(Home.connectionDB)).getAllUSByProject(module.exports.projectOpened);
   const listeHTML = document.getElementById('IssueList');
-  await listUS.forEach((us) =>{
-    listeHTML.innerHTML+='<li>'+us.toString()+'</li>';
-  });
+  const utilsForm = new UtilsForm();
+  const list = await document.createElement('p');
+  for (let i = 0; i < listUS.length; i++) {
+    const form = await utilsForm.getForm(document, 'form'+listUS[i].id, '/backlog', 'post');
+    await form.appendChild(await utilsForm.getTexte(document, listUS[i].toString()));
+    const button = await utilsForm.getInput(document, 'delete', 'submit');
+    button.value='Supprimer';
+    button.id='delete'+listUS[i].id;
+    const hiddenData = await utilsForm.getInput(document, 'data', 'hidden');
+    hiddenData.value=listUS[i].id;
+    await form.appendChild(button);
+    await form.appendChild(hiddenData);
+    await list.appendChild(form);
+  }
+  await listeHTML.appendChild(list);
 }
 function addButtonCreate(document) {
   const button = document.getElementById('AddUs');
